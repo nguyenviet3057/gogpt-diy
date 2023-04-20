26bc8dc9c96e2dc680792acdbdc6d03572cea06e
nguyenviet3057 <nguyenviet3057@gmail.com>
2023-04-20 14:59:29 +0700

    push code

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const Sidebar = () => {
     return (
-        <div>
-            
-        </div>
+        <Container fluid className='w-100 h-100 position-relative'>
+            <div className='w-100 h-100 side-bar'></div>
+            <h1 className='w-100 h-100 position-absolute d-flex align-items-center justify-content-center' style={{ top:'0px', left:'0px', color: 'red', filter: 'blur(2px)' }}>Censored</h1>
+        </Container>
     );
 }
 
